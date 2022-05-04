afb91c3d14fe7580f3dcbb565b442708af4a1e9c
sagi goldfarb <84674218+sagiGold@users.noreply.github.com>
2022-05-04 20:14:44 +0300

    Working on Vehicle Factory

--- Ex03.GarageLogic/VehicleFactory.cs
+++ Ex03.GarageLogic/VehicleFactory.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Reflection;
 
 namespace Ex03.GarageLogic
 {
+
     class VehicleFactory
     {
+        private List<Vehicle> m_VehiclesList;
+
+        private void buildVehicleList()
+        {
+            MethodInfo[] allMethods = this.GetType().GetMethods();
+            foreach (MethodInfo methodInfo in allMethods)
+            {
+                if (methodInfo.ReturnType is Vehicle)
+                {
+                    m_VehiclesList.Add(methodInfo.Invoke(this, null) as Vehicle);
+                }
+            }
+        }
+
+        public Motorbike BuildGasMotorbike()
+        {
+            int numOfWheels = 2;
+            float maxPressure = 31;
+            float engineCC = 6.2F;
+            eFuelType fuelType = eFuelType.Octan98;
+
+            return new Motorbike(numOfWheels, maxPressure, engineCC, fuelType);
+        }
+
+        public Motorbike BuildElectricMotorbike()
+        {
+            int numOfWheels = 2;
+            float maxPressure = 31;
+            float batteryLife = 2.5F;
+
+            return new Motorbike(numOfWheels, maxPressure, batteryLife);
+        }
     }
 }
